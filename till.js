f9789321c3e6a324572cbcb33a19efcb9d6e174c
Linton Mackereth <linton@Lintons-MBP.home>
2017-06-26 19:05:44 +0100

    added fruit products

---
+++ till.js
@@ -0,0 +1,21 @@
+var apples = {
+    name: 'Apples',
+    price: 0.99
+}
+
+var pears = {
+    name: 'Pears',
+    price: 1.99
+}
+
+var grapes = {
+    name: 'Grapes',
+    price: 1.49
+}
+
+var oranges = {
+    name: 'Oranges',
+    price: 1.20
+}
+
+var products = [apples, pears, grapes, oranges]
